19b413f13e171d6736ea35f0cf88b82fb6f58761
hyperrex <mrwendell@gmail.com>
2018-12-09 22:27:42 -0800

    Add html and validation for SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -31,17 +31,101 @@ class SignUpForm extends React.Component {
     return (
       <div>
         <Navbar />
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+
+        <div
+          className="container border border-primary rounded my-5 w-50"
+          id="sign-up-container"
+        >
+          <form id="sign-up-form" className="form text-center">
+            <h4 className="m-1 text-center">
+              Not registered yet? Just enter your information below.
+            </h4>
+            <div className="row">
+              <div className="form-group text-center col-6">
+                <label for="first-name" className="control-label" />
+                <input
+                  required
+                  type="first-name"
+                  className="form-control text-center text-muted"
+                  id="first-name"
+                  placeholder="First Name"
+                  required
+                />
+              </div>
+
+              <div className="form-group text-center col-6">
+                <label for="last-name" className="control-label" />
+                <input
+                  required
+                  type="last-name"
+                  className="form-control text-center text-muted"
+                  id="last-name"
+                  placeholder="Last Name"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group text-center col-12">
+                <label for="email" className="control-label" />
+                <input
+                  required
+                  type="email"
+                  className="form-control text-center text-muted"
+                  id="create-email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group text-center col-6">
+                <label for="password" className="control-label" />
+                <input
+                  required
+                  type="password"
+                  className="form-control text-center text-muted"
+                  id="sign-up-password"
+                  placeholder="Password"
+                  required
+                  pattern="(?=^.{8,30}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                />
+                <small id="passwordHelpInline" className="text-center">
+                  Must be 8-30 characters long and include upper and lower case
+                  letters, a number, and a special character.
+                </small>
+              </div>
+
+              <div className="form-group text-center col-6">
+                <label for="verify-password" className="control-label" />
+                <input
+                  required
+                  type="password"
+                  className="form-control text-center text-muted"
+                  id="verify-password"
+                  placeholder="Re-Enter Password"
+                  required
+                  pattern="(?=^.{8,30}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                />
+                <small id="passwordHelpInline" className="text-center">
+                  Re-entry password to verify
+                </small>
+              </div>
+            </div>
+
+            <div className="form-group text-center">
+              <button
+                type="submit"
+                id="submit-button"
+                className="btn btn-lg btn-primary"
+              >
+                <i className="fa fa-user-circle" /> Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
